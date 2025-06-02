7bcb70b4fb6ef98c93e71703c7cc0bac5d538de6
fawaazbutt <fawaazbutt@gmail.com>
2025-06-02 23:07:26 +0500

    title fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { Inter } from "next/font/google"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Unrender Studio | Fawaz Butt",
+  title: "Fawaz Butt | Unrender Studio",
   description: "Creative Video Production Portfolio",
 
   
